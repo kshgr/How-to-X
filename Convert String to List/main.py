536ffbac83294bfdc46d86eab32a5533ade35994
Kushagra Mittal <kushagra.mittal@ucdconnect.ie>
2023-03-31 20:20:24 +0100

    Create main.py

---
+++ Convert String to List/main.py
@@ -0,0 +1,20 @@
+string = "This is   a string."
+L = []
+
+L = string.split()
+
+print(L)
+
+# Seperator
+
+S = []
+S = string.split(" ")
+
+print(S)
+
+# Max Split
+
+M = []
+M = string.split(" ", -1)
+
+print(M)
